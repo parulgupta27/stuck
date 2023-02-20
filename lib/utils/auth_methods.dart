8b16e887bd9542712cb1d11ba146756b653c07b4
Parul Gupta <parulgupta0227@gmail.com>
2023-02-20 15:43:31 +0530

    auth logic

--- lib/utils/auth_methods.dart
+++ lib/utils/auth_methods.dart
@@ -1,11 +1,36 @@
+import 'dart:typed_data';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
 class AuthMethod {
 var _auth=FirebaseAuth.instance;
-String? email;
-String ?password;
-String ?name;
-XFile ?image;
-
+var _firestore=FirebaseFirestore.instance;
+var _storage=FirebaseStorage.instance;
+signUpUser(String email,String password,String name,Uint8List img)async{
+  if(email.length==0||password.length==0||name.length==0||img==null)return;
+var credentials=await _auth.createUserWithEmailAndPassword(email: email, password: password);
+User? user=credentials.user;
+await user!.updateDisplayName(name);
+await user.sendEmailVerification();
+var ref=await _storage.ref("profilepics").child(user.uid);
+await ref.putData(img);
+String url=await ref.getDownloadURL();
+var refernece=await _firestore.collection("users").doc(user.uid);
+refernece.set({
+  "name":name,
+  "id":user.uid,
+  "email":email,
+  "profilepicUrl":url,
+});
+}
+signInUser(String email,String password)async{
+  if(email.length==0||password.length==0)return;
+var credentials=await _auth.signInWithEmailAndPassword(email: email, password: password);
+User? user=credentials.user;
+if(!user!.emailVerified){
+  user.sendEmailVerification();
+}
+}
 }
